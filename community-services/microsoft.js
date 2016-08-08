bc5306c937b07b17fadcfafacf0bc2ba664139bc
Grant Hobbs <ghobbsus@gmail.com>
2016-08-08 17:20:22 -0500

    Switched Microsoft package from ghobbs:microsoft to ghobbs:microsoft2.

--- community-services/microsoft.js
+++ community-services/microsoft.js
@@ -3,8 +3,8 @@ if (Meteor.isClient) {
     if (!Meteor.userId()) {
       throw new Meteor.Error(402, 'Please login to an existing account before link.');
     }
-    if(!Package['ghobbs:microsoft']) {
-      throw new Meteor.Error(403, 'Please include ghobbs:microsoft package');
+    if(!Package['ghobbs:microsoft2']) {
+      throw new Meteor.Error(403, 'Please include ghobbs:microsoft2 package');
     }
 
     if (! callback && typeof options === "function") {
